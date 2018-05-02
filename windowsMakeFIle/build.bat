b88da17f7daf8939fba58626ccfc1ba990804bed
Joseph Garner <s3550293@student.rmit.edu.au>
2018-05-02 19:39:44 +1000

    Changed make Files for new file names

--- windowsMakeFIle/build.bat
+++ windowsMakeFIle/build.bat
@@ -5,7 +5,6 @@ mkdir build
 
 pushd build
 
-cl ../prim.c ../sin.c ../boat.c ../island.c ../projectile.c user32.lib gdi32.lib opengl32.lib glu32.lib glut32.lib
-REM cl ../run.c user32.lib gdi32.lib opengl32.lib glu32.lib glut32.lib
+cl ../main.c user32.lib gdi32.lib opengl32.lib glu32.lib glut32.lib
 
 popd
